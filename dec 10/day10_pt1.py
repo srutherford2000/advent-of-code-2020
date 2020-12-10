7f41da82229b46c3bc1f7c016db3786f88e2a32f
Sarah Rutherford <sarah.rutherford@westpoint.edu>
2020-12-10 18:50:16 -0500

    day 10

--- dec 10/day10_pt1.py
+++ dec 10/day10_pt1.py
@@ -7,33 +7,20 @@ for line in in_file:
     lines.append(int(float(line.strip())))
 
 in_file.close()
-lines.append(0) #change to 75 when using the second half
 
 lines.sort()
 
-def path(cur_ind):
-    val = lines[cur_ind]
-    i = cur_ind+1
-    next_steps=[]
-    next_steps_vals = []
-    paths = 0
-    while (i<len(lines) )and (i-cur_ind <=3):
-        next_val = lines[i]
-        if next_val - val <= 3:
-            next_steps.append(i)
-            next_steps_vals.append(next_val)
-        else: 
-           break
-        i += 1
-    if (cur_ind >= len(lines)) or next_steps_vals == []:
-        return 1
-    else:
-        for thing in next_steps:
-            x = path(thing)
-            paths += x
-        return paths
-    
+num_of_threes = 1 #accounts for the one at the end
+num_of_ones = 0
 
+last_num = 0
 
-y = path(0)
-print(y)
+for num in lines:
+    dif = num - last_num
+    if (dif == 3):
+        num_of_threes += 1
+    elif (dif ==1):
+        num_of_ones += 1
+    last_num = num
+
+print("ANS is:", num_of_threes * num_of_ones)
